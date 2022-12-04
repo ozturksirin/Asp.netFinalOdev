cd4100ebcdab760bae511fcbec699dca809c4f61
ozturk sirin <ozturk2002.zri@gmail.com>
2022-12-04 16:57:35 +0300

    input page

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -17,7 +17,7 @@ namespace BlogMvcApp.Controllers
         // GET: Blog
         public ActionResult Index()
         {
-            var bloglar = db.Bloglar.Include(b => b.Category);
+            var bloglar = db.Bloglar.Include(b => b.Category).OrderByDescending(i=>i.EklemeTarihi);
             return View(bloglar.ToList());
         }
 
@@ -48,10 +48,12 @@ namespace BlogMvcApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Baslik,Aciklama,Resim,Icerik,EklemeTarihi,Onay,Anasayfa,CategoryId")] Blog blog)
+        public ActionResult Create([Bind(Include = "Baslik,Aciklama,Resim,Icerik,CategoryId")] Blog blog)
         {
             if (ModelState.IsValid)
             {
+                blog.EklemeTarihi = DateTime.Now;
+                
                 db.Bloglar.Add(blog);
                 db.SaveChanges();
                 return RedirectToAction("Index");
