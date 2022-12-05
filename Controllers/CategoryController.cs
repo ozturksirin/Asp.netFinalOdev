a13ada3c1231a42d37afd73d06dc39ae6fba9546
ozturk sirin <ozturk2002.zri@gmail.com>
2022-12-05 21:27:04 +0300

    Blog Details

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -90,6 +90,8 @@ namespace BlogMvcApp.Controllers
             {
                 db.Entry(category).State = EntityState.Modified;
                 db.SaveChanges();
+
+                TempData["Kategori"] = category;
                 return RedirectToAction("Index");
             }
             return View(category);
